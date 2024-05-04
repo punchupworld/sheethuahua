685196128d1118c59c4c6c04e73fc4ef0deea2aa
Th1nkK1D <witheep@gmail.com>
2024-05-04 14:41:30 +0700

    feat: spreadsheets can specify global options

--- src/spreadsheet.ts
+++ src/spreadsheet.ts
@@ -12,13 +12,14 @@ import type { TTable } from './table';
 export function Spreadsheet<T extends TTable<any, any>[]>(
 	sheetsId: string,
 	tables: [...T],
+	globalOptions: CSVParserOptions = {},
 ) {
 	const tablesSchema = Type.Intersect(tables);
 
 	return {
 		get<N extends Static<TKeyOf<typeof tablesSchema>>>(
 			tableName: N,
-			options?: CSVParserOptions,
+			options: CSVParserOptions = {},
 		): Promise<Static<TIndexFromPropertyKey<Intersect<T>, N>>[]> {
 			const columnsSchema = Type.Index(tablesSchema, [tableName]);
 
@@ -29,7 +30,7 @@ export function Spreadsheet<T extends TTable<any, any>[]>(
 			return parseCSVFromUrl(
 				`https://docs.google.com/spreadsheets/d/${sheetsId}/gviz/tq?tqx=out:csv&sheet=${tableName}`,
 				columnsSchema,
-				options,
+				{ ...globalOptions, ...options },
 			);
 		},
 	};
