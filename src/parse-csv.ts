532b74e7d6afb86fee94b92ab50f17c271e2c6fa
Th1nkK1D <witheep@gmail.com>
2024-10-25 15:16:13 +0700

    perf: add column property map in parseCsv

--- src/parse-csv.ts
+++ src/parse-csv.ts
@@ -5,6 +5,11 @@ import { checkColumnSchema, type TColumn } from './column';
 
 const ROW_INDEX_OFFSET = 1;
 
+interface ColumnProperty {
+	name: string;
+	index: number;
+}
+
 /**
  * Parse the CSV string according to the given table
  * @param content - A string of CSV file content
@@ -18,29 +23,42 @@ export function parseCsv<T extends TObject>(
 ): Static<T>[] {
 	const [headerRow, ...bodyRows] = csvParseRows(content);
 
+	const columnPropertyMap = new Map<string, ColumnProperty>(
+		Object.entries(schema.properties).reduce<[string, ColumnProperty][]>(
+			(pair, [key, value]) => {
+				if (checkColumnSchema(value)) {
+					const name = (value as TColumn).columnName ?? key;
+					const index = headerRow.indexOf(name);
+
+					if (index < 0) {
+						throw `Column "${name}" is referenced in the schema but does not found`;
+					}
+
+					pair.push([key, { name, index }]);
+				}
+				return pair;
+			},
+			[],
+		),
+	);
+
 	const parsedData = bodyRows.map((cols, rowIndex) =>
 		Object.entries(schema.properties).reduce<Record<string, unknown>>(
 			(obj, [key, value]) => {
 				if (checkColumnSchema(value)) {
-					const { columnName, ...columnSchema } = value as TColumn;
-					const columnIndex = headerRow.indexOf(columnName ?? key);
+					const column = columnPropertyMap.get(key) as ColumnProperty;
+					const trimmedValue = cols[column.index].trim();
 
-					if (columnIndex < 0) {
-						throw `Column "${columnName ?? key}" not found.`;
+					if (!trimmedValue && schema.required?.includes(key)) {
+						throw `Column ${column.name} cannot be empty (row ${rowIndex + ROW_INDEX_OFFSET})`;
 					}
 
-					const trimmedValue = cols[columnIndex].trim();
-
-					if (!trimmedValue.length && schema.required?.includes(key)) {
-						throw `Column ${columnName} cannot be empty (row ${rowIndex + ROW_INDEX_OFFSET})`;
-					}
-
-					if (trimmedValue.length) {
-						const parsedValue = Value.Convert(columnSchema, trimmedValue);
-						const error = Value.Errors(columnSchema, parsedValue).First();
+					if (trimmedValue) {
+						const parsedValue = Value.Convert(value, trimmedValue);
+						const error = Value.Errors(value, parsedValue).First();
 
 						if (error) {
-							throw `Unexpected value in the column ${columnName}, "${error.value}" is not a ${error.schema[Kind].toLowerCase()} (row ${rowIndex + ROW_INDEX_OFFSET})`;
+							throw `Unexpected value in the column ${column.name}, "${error.value}" is not a ${error.schema[Kind].toLowerCase()} (row ${rowIndex + ROW_INDEX_OFFSET})`;
 						}
 
 						obj[key] = parsedValue;
